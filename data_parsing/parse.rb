fd6518b653219706461355fee18074c0a856e28a
github username <github email address>
2015-05-28 16:14:30 -0400

    finished code

--- data_parsing/parse.rb
+++ data_parsing/parse.rb
@@ -6,14 +6,11 @@ class Parse
 
   def initialize()
     @json = JSON.parse(File.read("canvs_tweet_data.json"))
-    # binding.pry
-
   end
 
   def tweet_parser
     tweet_hash = {}
     index = 0
-
 
     json.each do |tweet|
       screen_name = tweet['user']['screen_name']
@@ -21,15 +18,12 @@ class Parse
       tweet_hash[screen_name] += 1
     end
 
-    top_five = tweet_hash.sort_by{|name, count| count}.reverse.slice(0,5)
-    # binding.pry
+     top_five = tweet_hash.sort_by{|name, count| count}.reverse.slice(0,5)
 
-      top_five.each do |name_n_tweets|
-        puts "#{name_n_tweets[0]}, #{name_n_tweets[1]}"
-      end
-
+    top_five.each do |name_n_tweets|
+      puts "#{name_n_tweets[0]}, #{name_n_tweets[1]}"
+    end
   end
-
 end
 
 parse = Parse.new
